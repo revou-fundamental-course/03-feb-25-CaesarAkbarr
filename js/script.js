558fbf899f85715c0e1497dd8445b9efbec11860
CaesarAkbarr <caesarakbar2006@gmail.com>
2025-02-10 23:01:08 +0700

    V1 done pertama

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-const inputCelcius = document.getElementById('celcius-textarea').value;
-const inputFahrenheit = document.getElementById('fahrenheit-textarea').value;
+function convertCelciusToFahrenheit() {
+  let celcius = document.getElementById("celcius-input").value;
+  let fahrenheit = (celcius * 9/5) + 32;
+  const output = document.getElementById("fahrenheit-textarea");
+  output.innerHTML = fahrenheit;
+};
+
+
